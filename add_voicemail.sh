7278d1f60d5acedeea922729ab84c6b2e06c1b1d
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2018-03-20 16:26:53 -0700

    fix formating

--- add_voicemail.sh
+++ add_voicemail.sh
@@ -3,11 +3,12 @@ audiofile="stream.part3.wav"
 mailbox=$1
 voicemail=$2
 
+# Get the nodeports for the services we need
 loadgeneratorsrv=$(kubectl get service loadgenerator -o jsonpath={.spec.ports[0].nodePort})
 storesrv=$(kubectl get service voicemailstore -o jsonpath={.spec.ports[0].nodePort})
 
 # Upload the audio
 curl -i -H "Content-Type:multipart/form-data" -X POST http://mycluster.icp:${storesrv}/api/v1/mailboxes/${mailbox}/voicemails -F file=@stream.part3.wav -F calldata='{"callerid":"+35318562445","duration":"0:10","messageid":"'${voicemail}'","name":"IBM Test","mailbox":"'${mailbox}'","cidnum":"+35318562445","cidname":"","date":"Monday, October 12, 2015 at 10:56:52 AM"};type=application/json'
 
-# Update the transcript
-curl -i -H 'Content-Type: application/json' -X POST http://mycluster.icp:${loadgeneratorsrv}/api/v1/voicemail --data @post.json
+# Trigger the transcribe service
+curl -i -H 'Content-Type: application/json' -X POST http://mycluster.icp:${loadgeneratorsrv}/api/v1/voicemail --data '{"callerid":"+35318562445","duration":"0:10","messageid":"'${voicemail}'","name":"IBM Test","mailbox":"'${mailbox}'","cidnum":"+35318562445","cidname":"","date":"Monday, October 12, 2015 at 10:56:52 AM"}'
